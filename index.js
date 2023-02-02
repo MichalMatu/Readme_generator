6975818c7d0e82b022d6cc6610b345c1cf5425c7
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-03 00:08:37 +0100

    updated comments for project

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// import
+// importing fs, path, and inquirer
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+// import generateMarkdown function from generateMarkdown.js
 const generateMarkdown = require("./utils/generateMarkdown");
-// array of questions for user
+// array of questions for user input for README file generation 
 const questions = [
     {
         type: "input",
@@ -58,15 +59,15 @@ const questions = [
         message: "Please provide your Github address."
     },
 ];
-
+// Get the data from the form and create a new object
 var data = {};
 
-// function to write README file
+// function to write README file using the data from the form 
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// function to initialize program
+// function to initialize program and prompt user for input then call the writeToFile function
 function init() {
     inquirer.prompt(questions).then((answers) => {
         writeToFile("README_gen.md", generateMarkdown(answers));
